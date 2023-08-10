2e9e078a0a7878ff2f8d471fce28409b703f7bb4
rafpang <rafibintangpagi@gmail.com>
2023-08-10 10:57:42 +0700

    added argon hash and migrated prisma

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable({})
 export class AuthService {
-  signin() {
-    return { msg: 'hellow signin' };
+  constructor(private prismaService: PrismaService) {}
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+    const user = await this.prismaService.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
+    delete user.hash;
+    return user;
   }
-  signup() {
+  signin(dto: AuthDto) {
     return { msg: 'hellow signup' };
   }
 }
--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,13 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
+@Global()
 @Module({
-  providers: [
-    PrismaService,
-    {
-      provide: 'DATABASE_URL',
-      useValue: process.env.DATABASE_URL,
-    },
-  ],
+  providers: [PrismaService],
+  exports: [PrismaService],
 })
 export class PrismaModule {}
