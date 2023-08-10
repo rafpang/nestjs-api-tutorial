edcfe113bc99bdd4644caa839978baa23d205609
rafpang <rafibintangpagi@gmail.com>
2023-08-10 12:41:16 +0700

    completed bookmarks module

--- src/auth/decorator/get-user.decorator.ts
+++ src/auth/decorator/get-user.decorator.ts
@@ -2,7 +2,7 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { Request } from 'express';
 
 export const GetUser = createParamDecorator(
-  (data: string | undefined, ctx: ExecutionContext) => {
+  (data: string | undefined | null, ctx: ExecutionContext) => {
     const request: Request = ctx.switchToHttp().getRequest();
     if (data) return request.user[data];
     else return request.user;
